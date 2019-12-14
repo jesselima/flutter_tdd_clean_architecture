cd7c193e81a7fd90d138c9f91ff7b5846f244b65
jesselima <jesselima.tech@gmail.com>
2019-12-13 22:02:53 -0300

    NumberTriviaState setup for use in Bloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_tdd_clean_architecture/features/number_trivia/domain/entities/number_trivia_entity.dart';
 
 abstract class NumberTriviaState extends Equatable {
- NumberTriviaState();
+ NumberTriviaState([List props = const <dynamic>[]]) : super(props);
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
-  @override
-  List<Object> get props => [];
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTriviaEntity numberTriviaEntity;
+  Loaded({ @required this.numberTriviaEntity }) : super([numberTriviaEntity]);
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+  Error({ @required this.message }) : super([message]);
 }
