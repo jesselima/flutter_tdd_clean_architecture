4a2d1c6516168b2f6560631ba46b235b7c934085
jesselima <jesselima.tech@gmail.com>
2019-12-30 21:46:05 -0300

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tdd_clean_architecture/features/number_trivia/presentation/pages/number_trivia_page.dart';
-import 'injection_container.dart' as di;
 
 void main() async {
-  /* Inject Dependencies from injection_container.dart
-  *  The runApp(MyApp()) will be called only after avery single dependency is initialized
-  * */
-  await di.init();
   runApp(MyApp());
 }
 
@@ -20,7 +14,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.green.shade800,
         accentColor: Colors.green.shade600
       ),
-      home: NumberTriviaPage()
+      home: Text("Hello!")
     );
   }
 }
